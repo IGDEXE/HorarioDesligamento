254492b02df64e03776ebf6a614880ae969825eb
Ivo Dias <igd753@gmail.com>
2019-11-18 17:04:58 -0300

    Deploy

---
+++ Verificar.bat
@@ -0,0 +1,11 @@
+@echo off  
+================================================================================  
+:: NOME   : TI - Gerenciar horario de funcionamento do PC
+:: AUTOR  : Ivo Dias  
+:: VERSAO : 281218 
+================================================================================
+::Titulo:
+title TI - Gerenciar horario de funcionamento do PC
+
+:: Inicia o programa 
+PowerShell.exe -ExecutionPolicy Bypass -File "C:\TI\ValidarHorario\InfoPC.ps1"
